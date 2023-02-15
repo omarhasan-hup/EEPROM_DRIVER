4a407a6d11fbc70b81c05aaab8a35695ed031115
Omar Hasan Shawky <engomar582@gmail.com>
2023-02-15 21:45:48 +0200

    This commit will help you to intialize the EEPROM to 0xff in each byte in case you hve defined the macro "First_Code"

--- SW/APP/main.c
+++ SW/APP/main.c
@@ -12,6 +12,16 @@
 #include "lcd.h"
 #include "Interrupt.h"
 #include "../HAL/Timer_driver/timer0.h"
+
+/*****************Definations****************************/
+//#define First_Code      0
+/*This Macro will be used to activate the set EEPROM function to make each byte in the EEprom equal 0xff*/
+
+
+
+
+
+/*******************************************************/
 /*******************************************************/
 /*Constants*/
 
@@ -20,24 +30,24 @@
 
 /*******************************************************/
 /*global variables*/
-
+#ifdef  First_Code
 	uint16 looper=0;
 	/*This looper variable will be used to loop on the first 11 bytes
 	 *in the EEPROM and store thir values then display them on the LCD*/
 	uint16 Data_Stored=0;
 	/*This variable will hold the data stored in the 10 byte block in the EEPROM
 	 *Byte per byte them display them */
-
-	uint16 First_Byte_Write=1;
-	uint16 First_Byte_Read=0;
+#endif
 
 
 /*******************************************************/
 
 /*functions prototypes*/
 void timer0_func(void);
-void Set_NVM(void);
+#ifdef  First_Code
+void Set_NVM_To_Max(void);
 void Check_NVM(void);
+#endif
 int main(void)
 {
 
@@ -46,33 +56,48 @@ int main(void)
 	/*call the function which will init the timer0 module with frequency of (Fcpu/8)*/
 	Timer0_Normal_Mode_Init(No_Prescaler);
 	Update_Ptr2appfunc(timer0_func);//Update the pointer to app function with the written function address
+
+#ifdef  First_Code
 	LCD_displayStringRowColumn(0,1,"Display trial");
-	LCD_clearScreen();
-	Set_NVM();
+	Set_NVM_To_Max();
 	LCD_displayStringRowColumn(0,1,"Intial the NVM=");
 	_delay_ms(2000);
 	LCD_clearScreen();
+#endif
+
+#ifdef  First_Code
 	Check_NVM();
 	LCD_displayStringRowColumn(0,1,"Check on NVM=");
 	_delay_ms(2000);
 	LCD_clearScreen();
+#endif
+
 
 
 
 	while(1)
 	{
-		if(looper==256)
+
+
+
+
+
+
+
+
+#ifdef First_Code
+		if(looper==512)
 		{
-			LCD_displayStringRowColumn(0,1,"Looper = 256");
+			LCD_displayStringRowColumn(0,1,"Looper = 512");
 			_delay_ms(2000);
 			LCD_clearScreen();
 			LCD_displayStringRowColumn(0,1,"Looper =");
 			LCD_intgerToString(looper);
 			_delay_ms(2000);
 		}
-		else if(looper<256)
+		else if(looper<512)
 		{
-			LCD_displayStringRowColumn(0,1,"Looper < 256");
+			LCD_displayStringRowColumn(0,1,"Looper < 512");
 			_delay_ms(2000);
 			LCD_clearScreen();
 			LCD_displayStringRowColumn(0,1,"Looper =");
@@ -81,7 +106,7 @@ int main(void)
 		}
 		else
 		{
-			LCD_displayStringRowColumn(0,1,"Looper > 256");
+			LCD_displayStringRowColumn(0,1,"Looper > 512");
 			_delay_ms(2000);
 			LCD_clearScreen();
 			LCD_displayStringRowColumn(0,1,"Looper =");
@@ -90,23 +115,14 @@ int main(void)
 		}
 		LCD_clearScreen();
 		LCD_displayStringRowColumn(0,1,"Add(0) =");
-		LCD_intgerToString(eeprom_read_word((uint16*)0));
+		LCD_intgerToString(eeprom_read_byte((uint8*)0));
 		_delay_ms(2000);
 
 		LCD_clearScreen();
-		LCD_displayStringRowColumn(0,1,"Add(257) =");
-		LCD_intgerToString(eeprom_read_word((uint16*)257));
+		LCD_displayStringRowColumn(0,1,"Add(512) =");
+		LCD_intgerToString(eeprom_read_byte((uint8*)512));
 		_delay_ms(2000);
-
-
-
-
-
-
-
-
-
-
+#endif
 
 	}
 	return 0;
@@ -121,32 +137,28 @@ void timer0_func(void)
 {
 
 }
-
+#ifdef  First_Code
 /*This function is used intialize the NVM with all addresses equal ZERO except the first addresse  */
 
-void Set_NVM(void)
+void Set_NVM_To_Max(void)
 {
-	eeprom_write_word((uint16*)0,(uint16)1);
-	for(uint16 i=1;i<=256;i++)
+	for(uint16 i=0;i<512;i++)
 	{
-		eeprom_write_word((uint16*)i,(uint16)0);
+		eeprom_write_byte((uint8*)i,(uint8)0xff);
 	}
 
 }
 
 void Check_NVM(void)
 {
-	Data_Stored=eeprom_read_word((uint16*)0);
-	if(Data_Stored==(uint16)1)
+
+	for(uint16 i=0;i<512;i++)
 	{
-		looper=1;
-	}
-	for(uint16 i=1;i<=256;i++)
-	{
-		Data_Stored=eeprom_read_word((uint16*)i);
-		if(Data_Stored==(uint16)0)
+		Data_Stored=eeprom_read_byte((uint8*)i);
+		if(Data_Stored==(uint8)0xff)
 		{
 			looper++;
 		}
 	}
 }
+#endif
